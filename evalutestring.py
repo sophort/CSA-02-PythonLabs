2f50279c8a207be20d6ba964c663a333c64cd4d3
altradCS <145883008+altradCS@users.noreply.github.com>
2024-01-23 20:42:48 +0700

    Create evalutestring.py

---
+++ evalutestring.py
@@ -0,0 +1,16 @@
+
+# Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+# Note that if the range is given in capital letters, return the string in capitals also!
+
+# Examples
+# "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+# "h-o" ➞ "hijklmno"
+# "Q-Z" ➞ "QRSTUVWXYZ"
+# "J-J" ➞ "J"
+# Notes a hyphen will separate the two letters in the string.
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+user_range = input("Enter a range of letters (e.g., a-z): ")
+
+print(result_string)
